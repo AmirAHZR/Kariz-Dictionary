37d7cca8f05141504490af167d36ec42cc06b16f
AmirAHZR <amirhosseinzareiifar@gmail.com>
2026-01-01 20:24:04 +0330

    selection Use languages best

--- modes/user/search.py
+++ modes/user/search.py
@@ -3,29 +3,9 @@ from document.languages_words import langs, list_lang
 from functions.filtering.word_filtering import filtering
 from functions.translate.translate import *
 def user_mode():
-    modes = functions.UI.load_languages.load_languages(list(list_lang))
-    breaking = False
-    while True:
-        
-        counter = 1
-        for mode in modes:
-            print(f"{counter}-{mode[0]} to {mode[1]} ")
-            counter += 1
-        print("Insert 0 to stop")
-        #ask for the mode
-        translate_number = int(input())
-        if not(translate_number):
-            break
-        while not(0 < translate_number <= counter):
-            if not(translate_number):
-                breaking = True
-                break 
-            print("Please enter a number within a certain interval")
-            translate_number = int(input())
-        if breaking:
-            break 
+    origin, des = functions.UI.load_languages.load_languages(list(list_lang))
+    
+    word = input()
 
-        word = input()
-        origin,destination = modes[translate_number -1][0],modes[translate_number-1][1]
-        result = translate(filtering(word), langs[origin], langs[destination])
-        return result, origin, word, langs
+    result = translate(filtering(word), langs[list_lang[origin]], langs[list_lang[des]])
+    return result, origin, word, langs
